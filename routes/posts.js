a73f7ad90662bd62b9cb045a8ae2f5acf6caf67a
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-10-28 23:59:42 +0700

    fix error on axios delete

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 //import { v4 as uuidv4 } from 'uuid';
 const Post = require("../models/post");
+const async = require("async");
 
 
 
@@ -66,9 +67,33 @@ router.put('/:postId', (req, res) => {
 });
 
 //delete post
-router.delete('/:postId',(req,res)=>{
+router.delete('/:postId/',(req,res)=>{
 
-  return res.send('update post'+ req.params.postId);
+  async.parallel(
+    {
+      post(callback){
+        Post.findById(req.params.postId).exec(callback);
+
+      }
+    },
+    (err,results) => {
+      if(err) {
+        return next(err);
+      }
+      //success
+      Post.findByIdAndRemove(req.params.postId,
+        (err)=> {
+          //if error happen when removing
+          if(err){
+            return next(err);
+          }
+          // Sucessfully remove then redirect
+          console.log('succesfully deleted')
+          res.status(200).end();
+        })
+
+    }
+  )
 })
 
 
