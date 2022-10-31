656ec5cf703b888ce606215652934e67ff17c7a9
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-10-31 10:06:07 +0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(cors())
 
 //set up mongodb connection with mongoose
 const mongoose = require("mongoose");
-const mongoDB = 'mongodb+srv://kurniadikevin:passwordapi@cluster0.ysz4u9p.mongodb.net/?retryWrites=true&w=majority'
-//const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const dev_db_url = 'mongodb+srv://kurniadikevin:passwordapi@cluster0.ysz4u9p.mongodb.net/?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
