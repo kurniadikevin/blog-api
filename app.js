5e79f7a45d590dfb31f42a6c4c35f82edef3784f
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-10-31 14:15:02 +0700

    cors header setting

--- app.js
+++ app.js
@@ -20,7 +20,18 @@ var commentRouter = require('./routes/comment');
 
 var app = express();
 app.use(cors());
-app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+       res.send(200);
+   } else {
+       next();
+   }
+  });
 
 //set up mongodb connection with mongoose
 const mongoose = require("mongoose");
