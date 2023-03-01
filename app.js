609d18d0c4876acfba99fbb61c7f33403308c8bf
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-03-01 12:35:27 +0700

    install boall body parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 var logger = require('morgan');
 var cors = require('cors');
 const session = require("express-session");
@@ -47,6 +48,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //passport local strategy method
